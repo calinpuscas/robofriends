a57e9bd01a6e6ff185c63bf47b84b31b889f91de
Calin Puscas <calinpuscas@gmail.com>
2021-05-30 14:56:57 +0300

    Updated code and improved card list.

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Jenny Sorrow",
+    username: "Fanny",
+    email: "sorrow@mail.com",
+  },
+  {
+    id: 2,
+    name: "Diddy Sorrow",
+    username: "Folly",
+    email: "sorrow@mail.com",
+  },
+  {
+    id: 3,
+    name: "Penny Sorrow",
+    username: "Ginny",
+    email: "sorrow@mail.com",
+  },
+  {
+    id: 4,
+    name: "Shinny Sorrow",
+    username: "Lonny",
+    email: "sorrow@mail.com",
+  },
+  {
+    id: 5,
+    name: "Bobby Sorrow",
+    username: "Danny",
+    email: "sorrow@mail.com",
+  },
+  {
+    id: 6,
+    name: "Rudy Sorrow",
+    username: "Sonny",
+    email: "sorrow@mail.com",
+  },
+];
